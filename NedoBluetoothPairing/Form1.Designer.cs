63c44ef30a2959aaeafeed41a5bdf7ae8ed614b7
Samet CETIN <sametcetin@yandex.com>
2021-09-28 13:15:53 +0300

    Proje gite eklendi.

---
+++ NedoBluetoothPairing/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿
+namespace NedoBluetoothPairing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.cbxSerialPorts = new System.Windows.Forms.ComboBox();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.lblPorts = new System.Windows.Forms.Label();
+            this.btnSearchDevice = new System.Windows.Forms.Button();
+            this.lbxFoundedItems = new System.Windows.Forms.ListBox();
+            this.lblSearchStatus = new System.Windows.Forms.Label();
+            this.txtPairingMacAddr = new System.Windows.Forms.TextBox();
+            this.lblParingDevMac = new System.Windows.Forms.Label();
+            this.lblFoundedItem = new System.Windows.Forms.Label();
+            this.btnPair = new System.Windows.Forms.Button();
+            this.lblPairingStatus = new System.Windows.Forms.Label();
+            this.btnInitValues = new System.Windows.Forms.Button();
+            this.pnlSearch = new System.Windows.Forms.Panel();
+            this.pnlPairing = new System.Windows.Forms.Panel();
+            this.btnTerminal = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.pnlSearch.SuspendLayout();
+            this.pnlPairing.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cbxSerialPorts
+            // 
+            this.cbxSerialPorts.FormattingEnabled = true;
+            this.cbxSerialPorts.Location = new System.Drawing.Point(46, 17);
+            this.cbxSerialPorts.Name = "cbxSerialPorts";
+            this.cbxSerialPorts.Size = new System.Drawing.Size(121, 21);
+            this.cbxSerialPorts.TabIndex = 0;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Image = ((System.Drawing.Image)(resources.GetObject("btnRefresh.Image")));
+            this.btnRefresh.Location = new System.Drawing.Point(173, 9);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(36, 35);
+            this.btnRefresh.TabIndex = 1;
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // lblPorts
+            // 
+            this.lblPorts.AutoSize = true;
+            this.lblPorts.Location = new System.Drawing.Point(9, 20);
+            this.lblPorts.Name = "lblPorts";
+            this.lblPorts.Size = new System.Drawing.Size(31, 13);
+            this.lblPorts.TabIndex = 2;
+            this.lblPorts.Text = "Ports";
+            // 
+            // btnSearchDevice
+            // 
+            this.btnSearchDevice.Location = new System.Drawing.Point(6, 6);
+            this.btnSearchDevice.Name = "btnSearchDevice";
+            this.btnSearchDevice.Size = new System.Drawing.Size(93, 23);
+            this.btnSearchDevice.TabIndex = 3;
+            this.btnSearchDevice.Text = "Search Devices";
+            this.btnSearchDevice.UseVisualStyleBackColor = true;
+            this.btnSearchDevice.Click += new System.EventHandler(this.btnSearchDevice_Click);
+            // 
+            // lbxFoundedItems
+            // 
+            this.lbxFoundedItems.FormattingEnabled = true;
+            this.lbxFoundedItems.Location = new System.Drawing.Point(6, 66);
+            this.lbxFoundedItems.Name = "lbxFoundedItems";
+            this.lbxFoundedItems.Size = new System.Drawing.Size(197, 95);
+            this.lbxFoundedItems.TabIndex = 4;
+            this.lbxFoundedItems.Click += new System.EventHandler(this.lbxFoundedItems_Click);
+            this.lbxFoundedItems.SelectedIndexChanged += new System.EventHandler(this.lbxFoundedItems_SelectedIndexChanged);
+            // 
+            // lblSearchStatus
+            // 
+            this.lblSearchStatus.AutoSize = true;
+            this.lblSearchStatus.Location = new System.Drawing.Point(105, 11);
+            this.lblSearchStatus.Name = "lblSearchStatus";
+            this.lblSearchStatus.Size = new System.Drawing.Size(58, 13);
+            this.lblSearchStatus.TabIndex = 5;
+            this.lblSearchStatus.Text = "Search Init";
+            // 
+            // txtPairingMacAddr
+            // 
+            this.txtPairingMacAddr.Location = new System.Drawing.Point(5, 34);
+            this.txtPairingMacAddr.Name = "txtPairingMacAddr";
+            this.txtPairingMacAddr.Size = new System.Drawing.Size(148, 20);
+            this.txtPairingMacAddr.TabIndex = 6;
+            // 
+            // lblParingDevMac
+            // 
+            this.lblParingDevMac.AutoSize = true;
+            this.lblParingDevMac.Location = new System.Drawing.Point(2, 18);
+            this.lblParingDevMac.Name = "lblParingDevMac";
+            this.lblParingDevMac.Size = new System.Drawing.Size(143, 13);
+            this.lblParingDevMac.TabIndex = 7;
+            this.lblParingDevMac.Text = "Pairing Device MAC Address";
+            // 
+            // lblFoundedItem
+            // 
+            this.lblFoundedItem.AutoSize = true;
+            this.lblFoundedItem.Location = new System.Drawing.Point(2, 48);
+            this.lblFoundedItem.Name = "lblFoundedItem";
+            this.lblFoundedItem.Size = new System.Drawing.Size(74, 13);
+            this.lblFoundedItem.TabIndex = 8;
+            this.lblFoundedItem.Text = "FoundedItems";
+            // 
+            // btnPair
+            // 
+            this.btnPair.Location = new System.Drawing.Point(5, 60);
+            this.btnPair.Name = "btnPair";
+            this.btnPair.Size = new System.Drawing.Size(75, 23);
+            this.btnPair.TabIndex = 9;
+            this.btnPair.Text = "Pair";
+            this.btnPair.UseVisualStyleBackColor = true;
+            this.btnPair.Click += new System.EventHandler(this.btnPair_Click);
+            // 
+            // lblPairingStatus
+            // 
+            this.lblPairingStatus.AutoSize = true;
+            this.lblPairingStatus.Location = new System.Drawing.Point(97, 65);
+            this.lblPairingStatus.Name = "lblPairingStatus";
+            this.lblPairingStatus.Size = new System.Drawing.Size(56, 13);
+            this.lblPairingStatus.TabIndex = 10;
+            this.lblPairingStatus.Text = "Pairing Init";
+            // 
+            // btnInitValues
+            // 
+            this.btnInitValues.Location = new System.Drawing.Point(312, 17);
+            this.btnInitValues.Name = "btnInitValues";
+            this.btnInitValues.Size = new System.Drawing.Size(78, 43);
+            this.btnInitValues.TabIndex = 11;
+            this.btnInitValues.Text = "Load Init Values";
+            this.btnInitValues.UseVisualStyleBackColor = true;
+            this.btnInitValues.Click += new System.EventHandler(this.btnInitValues_Click);
+            // 
+            // pnlSearch
+            // 
+            this.pnlSearch.Controls.Add(this.lblFoundedItem);
+            this.pnlSearch.Controls.Add(this.lbxFoundedItems);
+            this.pnlSearch.Controls.Add(this.btnSearchDevice);
+            this.pnlSearch.Controls.Add(this.lblSearchStatus);
+            this.pnlSearch.Location = new System.Drawing.Point(11, 50);
+            this.pnlSearch.Name = "pnlSearch";
+            this.pnlSearch.Size = new System.Drawing.Size(210, 166);
+            this.pnlSearch.TabIndex = 12;
+            // 
+            // pnlPairing
+            // 
+            this.pnlPairing.Controls.Add(this.lblPairingStatus);
+            this.pnlPairing.Controls.Add(this.btnPair);
+            this.pnlPairing.Controls.Add(this.lblParingDevMac);
+            this.pnlPairing.Controls.Add(this.txtPairingMacAddr);
+            this.pnlPairing.Location = new System.Drawing.Point(227, 79);
+            this.pnlPairing.Name = "pnlPairing";
+            this.pnlPairing.Size = new System.Drawing.Size(170, 100);
+            this.pnlPairing.TabIndex = 13;
+            // 
+            // btnTerminal
+            // 
+            this.btnTerminal.Location = new System.Drawing.Point(315, 193);
+            this.btnTerminal.Name = "btnTerminal";
+            this.btnTerminal.Size = new System.Drawing.Size(75, 23);
+            this.btnTerminal.TabIndex = 14;
+            this.btnTerminal.Text = ">>>";
+            this.btnTerminal.UseVisualStyleBackColor = true;
+            this.btnTerminal.Click += new System.EventHandler(this.btnTerminal_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(6, 222);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(385, 185);
+            this.richTextBox1.TabIndex = 15;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(398, 411);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnTerminal);
+            this.Controls.Add(this.pnlPairing);
+            this.Controls.Add(this.pnlSearch);
+            this.Controls.Add(this.btnInitValues);
+            this.Controls.Add(this.lblPorts);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.cbxSerialPorts);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.pnlSearch.ResumeLayout(false);
+            this.pnlSearch.PerformLayout();
+            this.pnlPairing.ResumeLayout(false);
+            this.pnlPairing.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbxSerialPorts;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Label lblPorts;
+        private System.Windows.Forms.Button btnSearchDevice;
+        private System.Windows.Forms.ListBox lbxFoundedItems;
+        private System.Windows.Forms.Label lblSearchStatus;
+        private System.Windows.Forms.TextBox txtPairingMacAddr;
+        private System.Windows.Forms.Label lblParingDevMac;
+        private System.Windows.Forms.Label lblFoundedItem;
+        private System.Windows.Forms.Button btnPair;
+        private System.Windows.Forms.Label lblPairingStatus;
+        private System.Windows.Forms.Button btnInitValues;
+        private System.Windows.Forms.Panel pnlSearch;
+        private System.Windows.Forms.Panel pnlPairing;
+        private System.Windows.Forms.Button btnTerminal;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ NedoBluetoothPairing/Form1.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+namespace NedoBluetoothPairing
+{
+    public partial class Form1 : Form
+    {
+
+        SerialPort serialPort;
+        bool connOk = false;
+        bool Exit = false;
+        int DelayTime = 400;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Exit = true;
+            if (serialPort != null && serialPort.IsOpen)
+                serialPort.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Height = 260;
+            this.Text = Application.ProductName + " | " + Application.ProductVersion;
+            
+            SearchPorts();
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            SearchPorts();
+        }
+
+        private void SearchPorts()
+        {
+            cbxSerialPorts.Items.Clear();
+
+            string[] ports = SerialPort.GetPortNames();
+            var portName = SerialPort.GetPortNames();
+            foreach (string port in ports)
+            {
+                cbxSerialPorts.Items.Add(port);
+            }
+            cbxSerialPorts.SelectedIndex = cbxSerialPorts.Items.Count - 1;
+        }
+
+        private void btnSearchDevice_Click(object sender, EventArgs e)
+        {
+            pnlPairing.Enabled = false;
+            btnInitValues.Enabled = false;
+            btnSearchDevice.Enabled = false;
+
+            lbxFoundedItems.Items.Clear();
+
+            serialPort = new SerialPort();
+            if (string.IsNullOrEmpty(cbxSerialPorts.Text))
+            {
+                MessageBox.Show("Select port");
+                return;
+            }
+
+            if (!serialPort.IsOpen)
+                connOk = ConnectToPort();
+
+            if (connOk)
+            {
+                SearchDevices();
+                serialPort.Close();
+            }
+            pnlPairing.Enabled = true;
+            btnInitValues.Enabled = true;
+            btnSearchDevice.Enabled = true;
+        }
+
+        private bool ConnectToPort()
+        {
+            try
+            {
+                serialPort.PortName = cbxSerialPorts.Text;
+                serialPort.BaudRate = 9600;
+                serialPort.Parity = Parity.None;
+                serialPort.DataBits = 8;
+                serialPort.StopBits = StopBits.One;
+                serialPort.Handshake = Handshake.None;
+                serialPort.ReadTimeout = 80;
+                serialPort.WriteTimeout = 80;
+
+                serialPort.Open();
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private void SearchDevices()
+        {
+            string readVal = "1";
+            bool cr = false;
+            bool lf = false;
+            char byteData;
+            string strData = "";
+
+            lblSearchStatus.Text = "Searching...";
+
+            serialPort.WriteLine("AT");
+            System.Threading.Thread.Sleep(DelayTime);
+            serialPort.WriteLine("AT");
+            System.Threading.Thread.Sleep(DelayTime);
+            serialPort.WriteLine("AT");
+            System.Threading.Thread.Sleep(DelayTime);
+
+            serialPort.WriteLine("ROLE=M");
+            System.Threading.Thread.Sleep(DelayTime);
+            serialPort.WriteLine("SEARCH=?");
+
+            while (!readVal.Contains("Device(s) Found") && !Exit)
+            {
+                while (!(cr || lf) & !Exit)
+                {
+                    try
+                    {
+                        byteData = (char)serialPort.ReadChar();
+                    }
+                    catch
+                    {
+                        byteData = '-';
+                    }
+
+                    if (byteData == '\r')
+                        cr = true;
+                    else if (byteData == '\n')
+                        lf = true;
+                    else
+                        if (byteData != '-')
+                            strData = strData + byteData.ToString();
+
+                    Application.DoEvents();
+                }
+
+                readVal = strData;
+                if (!string.IsNullOrEmpty(readVal))
+                    lbxFoundedItems.Items.Add(readVal);
+                Application.DoEvents();
+                strData = "";
+                cr = false; lf = false;
+
+                Application.DoEvents();
+            }
+            lblSearchStatus.Text = "Search finished.";
+        }
+
+        private void WaitMs(int ms)
+        {
+            for (int i =0;i<ms/10; i++)
+            {
+                System.Threading.Thread.Sleep(10);
+                Application.DoEvents();
+            }
+        }
+
+        private void lbxFoundedItems_Click(object sender, EventArgs e)
+        {
+            string MaccAddr = "";
+
+            if (lbxFoundedItems.SelectedIndex > -1)
+                MaccAddr = lbxFoundedItems.SelectedItem.ToString();
+            txtPairingMacAddr.Text = MaccAddr;
+        }
+
+        private void lbxFoundedItems_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPair_Click(object sender, EventArgs e)
+        {
+            pnlSearch.Enabled = false;
+            btnInitValues.Enabled = false;
+
+            string maccAddr = txtPairingMacAddr.Text;
+            maccAddr = maccAddr.Replace(" ", "");
+            if (maccAddr.Length != 12)
+            {
+                MessageBox.Show("Unknown address selected !");
+                return;
+            }              
+
+            serialPort = new SerialPort();
+            if (string.IsNullOrEmpty(cbxSerialPorts.Text))
+            {
+                MessageBox.Show("Select port");
+                return;
+            }
+
+            if (!serialPort.IsOpen)
+                connOk = ConnectToPort();
+
+            if (connOk)
+            {
+                PairDevices(maccAddr);
+                serialPort.Close();
+            }
+
+            pnlSearch.Enabled = true;
+            btnInitValues.Enabled = true;
+
+        }
+
+        private void PairDevices(string maccAddr)
+        {
+            lblPairingStatus.Text = "Pairing...";
+
+            serialPort.WriteLine("AT");
+            System.Threading.Thread.Sleep(DelayTime);
+            serialPort.WriteLine("AT");
+            System.Threading.Thread.Sleep(DelayTime);
+            serialPort.WriteLine("AT");
+            System.Threading.Thread.Sleep(DelayTime);
+
+            serialPort.WriteLine("ROLE=M");
+            System.Threading.Thread.Sleep(DelayTime);
+            serialPort.WriteLine("PIN=0000");
+            System.Threading.Thread.Sleep(DelayTime);
+            serialPort.WriteLine("ECHO=N");
+            System.Threading.Thread.Sleep(DelayTime);
+            serialPort.WriteLine("PROMPT=N");
+            System.Threading.Thread.Sleep(DelayTime);
+            serialPort.WriteLine("DISCOVER=N");
+            System.Threading.Thread.Sleep(DelayTime);
+            serialPort.WriteLine("DEVICE=" + maccAddr);
+            System.Threading.Thread.Sleep(DelayTime);
+            serialPort.WriteLine("RECONNECT=Y");
+            System.Threading.Thread.Sleep(DelayTime);
+            serialPort.WriteLine("CONNECT=DEVICE");
+            System.Threading.Thread.Sleep(DelayTime);
+
+            lblPairingStatus.Text = "Pairing finished";
+
+        }
+
+        private void btnInitValues_Click(object sender, EventArgs e)
+        {
+            pnlSearch.Enabled = false;
+            pnlPairing.Enabled = false;
+
+            serialPort = new SerialPort();
+            if (string.IsNullOrEmpty(cbxSerialPorts.Text))
+            {
+                MessageBox.Show("Select port");
+                return;
+            }
+
+            if (!serialPort.IsOpen)
+                connOk = ConnectToPort();
+
+            if (connOk)
+            {
+                LoadInitValues();
+                serialPort.Close();
+            }
+
+            pnlSearch.Enabled = true;
+            pnlPairing.Enabled = true;
+
+        }
+
+        private void LoadInitValues()
+        {
+            serialPort.WriteLine("AT");
+            System.Threading.Thread.Sleep(DelayTime);
+            serialPort.WriteLine("AT");
+            System.Threading.Thread.Sleep(DelayTime);
+            serialPort.WriteLine("AT");
+            System.Threading.Thread.Sleep(DelayTime);
+
+            serialPort.WriteLine("ROLE=M");
+            System.Threading.Thread.Sleep(DelayTime);
+            serialPort.WriteLine("PIN=0000");
+            System.Threading.Thread.Sleep(DelayTime);
+            serialPort.WriteLine("ECHO=Y");
+            System.Threading.Thread.Sleep(DelayTime);
+            serialPort.WriteLine("PROMPT=Y");
+            System.Threading.Thread.Sleep(DelayTime);
+            serialPort.WriteLine("DEVICE=xxxxxxxxxxxx");    //12 adet 0
+            System.Threading.Thread.Sleep(DelayTime);
+            serialPort.WriteLine("RECONNECT=N");
+            System.Threading.Thread.Sleep(DelayTime);
+            serialPort.WriteLine("DISCOVER=N");
+            System.Threading.Thread.Sleep(DelayTime);
+            serialPort.WriteLine("PROMPT=Y");
+            System.Threading.Thread.Sleep(DelayTime);
+            MessageBox.Show("Init values loaded.");
+        }
+
+        private void btnTerminal_Click(object sender, EventArgs e)
+        {
+            //if (btnTerminal.Text == ">>>")
+            //{
+            //    this.Height = 450;
+            //    btnTerminal.Text = "<<<";
+            //}
+            //else
+            //{
+            //    this.Height = 260;
+            //    btnTerminal.Text = ">>>";
+            //}
+        }
+
+    }
+}
